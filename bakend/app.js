2f949db33ba43fd7280632a3adb1352e7ff3eca8
williamRA25 <williamrodriguezavila43@gmail.com>
2025-08-07 17:35:33 -0500

    add mongoose connection

--- bakend/app.js
+++ bakend/app.js
@@ -1,6 +1,7 @@
 require("dotenv/config.js");
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -9,6 +10,12 @@ const PORT = process.env.PORT || 3001;
 app.get("/", (req, res) => {
   res.send("OK");
 });
-app.listen(PORT, () => {
-  console.log("listening at port", PORT);
+app.listen(PORT, async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("mongoose connection succes");
+    console.log("listening at port", PORT);
+  } catch (error) {
+    console.log(error);
+  }
 });
