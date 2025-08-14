00f541d03276c2f64505fa1821a58d6507a2e272
Ronalavila <ronalluisavilanori@gmail.com>
2025-08-13 23:28:27 -0500

    add models

---
+++ frontend/src/app/models/category.model.ts
@@ -0,0 +1,6 @@
+export interface Category {
+  _id: string;       // ID de la categoría
+  name: string;      // Nombre de la categoría
+  createdAt?: string; // Fecha de creación (opcional)
+  updatedAt?: string; // Fecha de actualización (opcional)
+}
