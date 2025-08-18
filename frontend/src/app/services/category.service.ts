4be4e5cfc55081fd52c7ae61f917d2e821fc2932
williamRA25 <williamrodriguezavila43@gmail.com>
2025-08-17 20:58:36 -0500

    add environment settings

--- frontend/src/app/services/category.service.ts
+++ frontend/src/app/services/category.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryService {
-  private apiUrl = 'http://localhost:3001/categories';
+  private apiUrl = `${environment.apiUrl}/categories`;
 
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<string[]> {
-    return this.http.get<any[]>(this.apiUrl).pipe(
-      map(data => data.map(item => item.name))
-    );
+    return this.http
+      .get<any[]>(this.apiUrl)
+      .pipe(map((data) => data.map((item) => item.name)));
   }
 }
